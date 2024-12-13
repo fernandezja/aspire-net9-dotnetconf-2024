ae70b70dd671bb17a62e7fd0fb007cf89da9c131
Jose A. Fernandez <fernandezja@gmail.com>
2024-12-13 07:38:28 -0300

    Example 10: AppHost project references

--- 10/Starwars.AppHost/Program.cs
+++ 10/Starwars.AppHost/Program.cs
@@ -4,11 +4,15 @@ var cacheredis = builder.AddRedis("cache-redis")
                         .WithImageTag("alpine");
 
 
-var starwarsapi = builder.AddProject<Projects.Starwars_Api>("starwars-api");
+var starwarsapi = builder.AddProject<Projects.Starwars_Api>("starwars-api")
+                    .WithReference(cacheredis)
+                    .WaitFor(cacheredis); ;
 
 builder.AddProject<Projects.Starwars_App>("starwars-app")
        .WithExternalHttpEndpoints() //Azure App Service publico
        .WithReference(starwarsapi)
-       .WaitFor(starwarsapi); 
+       .WaitFor(starwarsapi) 
+       .WithReference(cacheredis)
+       .WaitFor(cacheredis);
 
 builder.Build().Run();
